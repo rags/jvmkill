9c8d78bb4f1f962ffca73d1eca9f3670b30045bf
Raghunandan Ramakrishna Rao <raghunr@amazon.com>
2019-09-19 23:32:40 +0000

    print no of threads

--- UnableToCreateThread.java
+++ UnableToCreateThread.java
@@ -3,12 +3,13 @@ import java.lang.*;
 class UnableToCreateThread {
 
     public static void main(String a[]){
-	while(true){
+	for(int i=0;;i++){
 	    new Thread(() -> {
 			try {
 			    Thread.sleep(10000000);
 			} catch(InterruptedException e) { }        
 		    }).start();
+	    System.out.println("Created Thread #" + i);
 	}
  
   }
